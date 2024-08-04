5b91d170182b1e4945f41be4b6d9f2b5e03b5f39
Ajeet <>
2024-08-04 21:38:50 +0530

    updated configuration-1

--- kind_cluster.tf
+++ kind_cluster.tf
@@ -22,14 +22,6 @@ resource "kind_cluster" "default" {
       kubeadm_config_patches = [
         "kind: InitConfiguration\nnodeRegistration:\n  kubeletExtraArgs:\n    node-labels: \"ingress-ready=true\"\n"
       ]
-      extra_port_mappings {
-        container_port = 80
-        host_port      = 80
-      }
-      extra_port_mappings {
-        container_port = 443
-        host_port      = 443
-      }
     }
 
     node {
